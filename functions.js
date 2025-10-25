bfb137fd140dcd77a3c4e6e33db17d431a314517
nehanlkt <nehanlkt@gmail.com>
2025-10-25 18:46:29 +0530

    mm

---
+++ functions.js
@@ -0,0 +1,28 @@
+function nice(name) {
+    console.log("hii"+name)
+    console.log("hii"+name)
+    console.log("hii"+name)
+}    
+nice("neha")
+nice("oi")
+
+// function sum (a,b) {
+//     console.log(a+b)
+    
+// }
+// sum(2,3)
+
+function sum (a,b,c=2) {
+    return a+b+c
+    
+}
+
+result=sum(2,3)
+result3=sum(4,3)
+result2=sum(4,3,3)
+console.log("sum is"+result)
+console.log("sum is"+result3)
+console.log("sum is"+result2)
+
+
+
