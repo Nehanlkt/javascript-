c68a3362759df5f29bcf8c3b824de7695d3c6c21
nehanlkt <nehanlkt@gmail.com>
2025-10-25 15:24:24 +0530

    variable

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log("hii")
+
+let a=5;
+let b=6;
+let c ="hhh"
+
+console.log(a+b)
+console.log(typeof a,typeof b,typeof c)
+
+{
+    let a=66;
+
+    console.log(a)
+}
+
+console.log(a)
+
+// if var both op vl be 66 n 66 bcz at last a=66 considered as global
+// if let then first 66 later 5
+
+// type of null is object
+// obj is key value pair
+
+let o={
+   name:"neha",
+    "salary code":55,
+
+}
+console.log(o);
+o.salary="1crore"
+console.log(o);
+
+
+
+
+
