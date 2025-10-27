3dcf104900ca956c34be5557a7877e517fb147fb
nehanlkt <nehanlkt@gmail.com>
2025-10-27 19:44:24 +0530

    jj

---
+++ faultcalci.js
@@ -0,0 +1,33 @@
+// let random=Math.random()
+
+// let a=prompt("enter num1")
+// let b=prompt("enter opr")
+// let c=prompt("enter num2")
+
+// let obj={
+//     "+":"-",
+//     "*":"+",
+//     "-":"/",
+//     "/":"",
+// }
+
+// c=obj[c]
+
+
+
+
+// if(random>0.1){
+//     console.log('the result is ${a} ${b} ${c} ')
+//     alert('the result is ${eval(' ${a} ${b} ${c}')}')
+
+
+// }
+// else{
+
+
+// c=obj[c]
+// alert('the result is ${eval(' ${a} ${b} ${c}')}')
+
+// }
+
+// not solved yetcd
