11c6e36b096feaa6880f306ba01cb8126d655c29
nehanlkt <nehanlkt@gmail.com>
2025-10-27 19:40:07 +0530

    kk

---
+++ strings.js
@@ -0,0 +1,25 @@
+// strings are immutable
+console.log("strings"
+)
+let a="neha"
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+
+console.log(hiii"nehaaa")
+
+let name="neh"
+let friend="dog"
+console.log(name is $(name) friend is $(friend))  
+// above both single and double quotes csn be used
+console.log("name is" +name+ "friend is" +friend)
+
+let d="doggy"
+console.log(d.length)
+console.log(d.slice(1,3))
+console.log(d.trim())
+console.log(d.replace("g",1))
+console.log(d.concat(a))
+console.log(d.concat(a,"ohh"))
