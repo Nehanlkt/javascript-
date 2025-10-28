e955d927a9785b3d5a47cc773b0356466098707d
nehanlkt <nehanlkt@gmail.com>
2025-10-28 22:32:29 +0530

    kk

---
+++ id.js
@@ -0,0 +1,10 @@
+console.log("harry")
+let boxes=document.getElementsByClassName("box")
+console.log(boxes)
+
+boxes[2].style.backgroundColor="red"
+
+document.getElementById("red").style.backgroundColor="blue"
+
+document.querySelector(".box").style.backgroundColor="purple"
+// first elemt vl be applied
