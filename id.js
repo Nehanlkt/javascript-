b8407957c4d98ea2ba5a533aa9b587c284f03c44
nehanlkt <nehanlkt@gmail.com>
2025-10-29 18:14:51 +0530

    id

--- id.js
+++ id.js
@@ -8,3 +8,20 @@ document.getElementById("red").style.backgroundColor="blue"
 
 document.querySelector(".box").style.backgroundColor="purple"
 // first elemt vl be applied
+// for all elem use
+console.log(document.querySelectorAll(".box"))
+document.querySelectorAll(".box").forEach(e=>{
+    e.style.backgroundColor="green";
+})
+
+// dom are usefull when parent child r close to eachother
+
+document.getElementsByTagName("div")
+// shows all html collection including parent
+
+// matches- to check if elem matches given css selectors
+
+
+// closest
+
+// need to learn this
