0920c166da5d5d930f7edb190d1fe75cf1db3123
nehanlkt <nehanlkt@gmail.com>
2025-11-01 23:01:25 +0530

    not completed

--- event.js
+++ event.js
@@ -5,3 +5,14 @@ bttn.addEventListener("click",()=>{
     document.querySelector(".box").innerHTML="<b> clicked </b> enjoy!"
 
 })
+
+// many more events search in google
+
+bttn.addEventListener("contextmenu",()=>{
+    alert("dont right click")
+// if right click on btn
+
+})
+
+
+
