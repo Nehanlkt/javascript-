a0a6c7e043d610626878ca042cdcb83182b68669
nehanlkt <nehanlkt@gmail.com>
2025-11-01 22:42:12 +0530

    more

---
+++ event.js
@@ -0,0 +1,7 @@
+let bttn=document.getElementById("btn")
+
+bttn.addEventListener("click",()=>{
+    alert("i was clicked")
+    document.querySelector(".box").innerHTML="<b> clicked </b> enjoy!"
+
+})
