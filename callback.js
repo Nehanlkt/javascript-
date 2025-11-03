c45e3f53c0dd55f202ee0e7796eb215c29b4c012
nehanlkt <nehanlkt@gmail.com>
2025-11-03 15:37:43 +0530

    clbcknw

--- callback.js
+++ callback.js
@@ -18,6 +18,28 @@ console.log("end")
 // order of execution neha,neh,end,hi,hlo
 
 
+// callback is the function passed as argument to another function
+
+function sum(a,b){
+    console.log(a+b);
+}
+function calculator(a,b,sumcl){
+    sumcl(a,b)
+
+}
+
+// calculator(1,2,(a,b)=>{
+//     console.log(a+b);
+// });
+
+// or
+
+calculator(1,2,sum);
+
+
+
+
+
 
 
 
