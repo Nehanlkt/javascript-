716e80ee6504715d7b2fd7b78af7482633f3aad6
nehanlkt <nehanlkt@gmail.com>
2025-11-05 22:33:36 +0530

    promisesdef

---
+++ promises.js
@@ -0,0 +1,4 @@
+// promise is for eventual completion of task.it is obj is js
+// it is sln to callback hell
+// let promise=new promise((resolve,reject)=>{...})
+// resolve n reject r the clback provided by js
