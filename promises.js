4d3d666ff7604ce976077bbb2710cae8701e73fc
nehanlkt <nehanlkt@gmail.com>
2025-11-07 10:34:50 +0530

    promise

--- promises.js
+++ promises.js
@@ -3,15 +3,27 @@
 // let promise=new promise((resolve,reject)=>{...})
 // resolve n reject r the clback provided by js
 // 3 states of promises-pending,resolve,reject
-let promise = new Promise((resolve, reject) => {
-    console.log("i am promise");
-    // resolve("done");
-    reject("some error");
-    // this gives error as some error
 
 
+// let promise = new Promise((resolve, reject) => {
+//     console.log("i am promise");
+//     // resolve("done");
+//     reject("some error");
+//     // this gives error as some error
+// });
 
+
+function getdata(dataid, getnextdata) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("data", dataid);
+            resolve("success");
+            if (getnextdata) {
+                getnextdata();
+            }
+        }
+    }, 5000);
 });
+}
 
-// need to do
-// need to do css exercises also
+
