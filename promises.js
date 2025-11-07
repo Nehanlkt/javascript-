65c435025b4e998f641a53b135bbbb4b34c6dc4a
nehanlkt <nehanlkt@gmail.com>
2025-11-07 11:03:59 +0530

    promise

--- promises.js
+++ promises.js
@@ -13,17 +13,40 @@
 // });
 
 
-function getdata(dataid, getnextdata) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log("data", dataid);
-            resolve("success");
-            if (getnextdata) {
-                getnextdata();
-            }
-        }
-    }, 5000);
-});
-}
 
+// function getdata(dataid, getnextdata) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("data", dataid);
+//             resolve("success");
+//             // reject("errrror")
+//             if (getnextdata) {
+//                 getnextdata();
+//             }
+
+//     }, 8000);
+// });
+// }
+
+// these promises r given by api
+// now we have to see how to use these problems
+// uses by .then() for resolved & .catch()
+
+const getpromise = () => {
+    return new Promise((resolve, reject) => {
+        console.log("i am promise");
+        // resolve("done");
+        reject("some error");
+        // this gives error as some error
+    });
+};
+
+let promise=getpromise();
+promise.then((res)=>{
+    console.log("promise fullfilled",res);
+});
+promise.catch((err)=>{
+    console.log("rejected",err);
+}
+)
 
