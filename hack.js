9a5b4ffa0b13dc3c2a0b4136c5776aa7d941c1c4
nehanlkt <nehanlkt@gmail.com>
2025-11-08 20:49:23 +0530

    hacking

---
+++ hack.js
@@ -0,0 +1,10 @@
+const getRandom=()=>{
+    return new Promise((resolve,reject)=>{
+        timeout=1+6*Math.random();
+        setTimeout(() => {
+            resolve()
+        }, 2000);
+    }
+
+)}
+
---
+++ ques.js
@@ -0,0 +1,21 @@
+// ques 1
+let std=["nn","ss","kl","pol","klo","pola","koga"]
+let houses=[]
+for (const std of houses) {
+    if(std.length<4){
+        houses.push("red");
+    }
+    else if(std.length<8){
+         houses.push("green");
+    }
+    else{
+        houses.push("blue");
+
+        
+    }
+    
+}
+console.log(houses)
+
+// q2
+
