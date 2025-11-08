6fe486dbb3d3ca72d2cdd8f07d97e5c06e32eadd
nehanlkt <nehanlkt@gmail.com>
2025-11-08 15:23:07 +0530

    obj constructor

--- classes.js
+++ classes.js
@@ -1,17 +1,25 @@
 // classes : is a prgm code template for creating objects
 // those obj vl have some state(variables) n fn inside it
 class Car{
+    constructor(brand){
+        console.log("creating new obj")
+         this.brandname=brand;
+    }
     start(){
         console.log("stsrt")
     }
     stop(){
         console.log("stop")
     }
-    setbrand(brand){
-        this.brandname=brand;
-    }
+    // setbrand(brand){
+    //     this.brandname=brand;
+    //     // instead of brandname any name can be given even brand can also be given
+    // }
 }
 // obj
-let bmw=new Car();
-bmw.setbrand("bmw");
-let lexus=new Car();
+let bmw=new Car(bmw);  //constructer vl invoke here as obj created
+// bmw.setbrand("bmw");
+let lexus=new Car(lexus); //constructer vl invoke here as obj created
+
+// constructor method used in classes,automatically invoked by new
+
